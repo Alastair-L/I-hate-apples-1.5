38496335f973f5a32c3110023266006c93f6f4d8
Alastair Locke <lockealastair@gmail.com>
2024-01-14 19:07:25 +1100

    Unfinished shelf

--- Scenes/Level 1/add_top_shelf.gd
+++ Scenes/Level 1/add_top_shelf.gd
@@ -3,18 +3,23 @@ extends TileMap
 var base_layer = 0
 var source_id = 0
 var overlay_layer = 1
+var SOURCE_ID = 0
+var BASE_TILE_ATLAS_ID = Vector2i(0,4)
+var TOP_SHELF_TILE_ATLAS_ID = Vector2i(0,5)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var atlas_id = Vector2i(0,4)
-	TileMap
-	var hi = tile_set.get_alternative_level_tile_proxy(atlas_id)
-	print(hi)
-	#add_layer(overlay_layer)
-	#set_layer_z_index(overlay_layer, 2)
+	# Get tile ids 
+	var tile_atlas_source = tile_set.get_source(0)
+	var base_tile_id = tile_atlas_source.get_tile_at_coords(BASE_TILE_ATLAS_ID)
+	var top_shelf_tile_id = tile_atlas_source.get_tile_at_coords(TOP_SHELF_TILE_ATLAS_ID)
+	
+	add_layer(overlay_layer)
+	set_layer_z_index(overlay_layer, 2)
 
-	#var cells_to_add_overlay_to = get_used_cells_by_id(base_layer, source_id, base_atlas_coords)
-	#set_cell(overlay_layer, cell, source_id, overlay_atlas_coords, alternative_tile)
+	var cells_to_add_overlay_to = get_used_cells_by_id(base_layer, SOURCE_ID, BASE_TILE_ATLAS_ID)
+	for cell in cells_to_add_overlay_to:
+		set_cell(overlay_layer, cell, SOURCE_ID, TOP_SHELF_TILE_ATLAS_ID)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
