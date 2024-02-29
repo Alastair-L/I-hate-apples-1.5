22b32d12a87e42947e350c7f2091dcf03030f864
rednazvsoob <zandervonstiegler@gmail.com>
2024-02-29 22:15:39 +1100

    Finalise for release

--- Scripts/GameTimer.gd
+++ Scripts/GameTimer.gd
@@ -1,6 +1,7 @@
 extends Timer
 
-@export var TIME_LOSS = 5 
+@export var TIME_LOSS = 3 
+
 
 func lose_time():
 	# Decrease timer by x seconds
@@ -9,3 +10,9 @@ func lose_time():
 		stop()
 	else:
 		start(time_left - TIME_LOSS)
+
+func _on_timeout():
+	get_tree().reload_current_scene()
+
+func _ready():
+	timeout.connect(_on_timeout)
--- Scripts/StartGameButton.gd
+++ Scripts/StartGameButton.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Scenes/test_scene.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://Scenes/Levels/level1.tscn") # Replace with function body.
